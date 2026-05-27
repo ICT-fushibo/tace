2b2214e040207b559d6bb4f92072ec1299827ba0
xuzemin <xvzemin123@outlook.com>
2026-05-28 00:03:01 +0800

    Add anti ineraction for SO3VstpGrid

--- tace/models/so2/README.md
+++ tace/models/so2/README.md
@@ -5,4 +5,5 @@ theories of QHNetV2. We also refactor `SO2Linear` introduced in eSCN
 to satisfy our specific needs. If you use the `so2` code from TACE, you should 
 explicitly indicate that it was obtained from TACE.
 
-- `Jd.pt` and `so3.py`: copied from EquiformerV3 without modification.
+- `so3.py`: Based on EquiformerV3. `SO3VstpGrid` are introduced by TACE.
+
--- tace/models/so2/__init__.py
+++ tace/models/so2/__init__.py
@@ -1,6 +1,6 @@
 from .so2 import SO2Linear, SO2Gate, SO2Norm, SO2ComplexMul
 from .so2 import SO2uuuTensorProduct as SO2TensorProduct
-from .so3 import SO3Rotation, SO3Grid, SO3Linear
+from .so3 import SO3Rotation, SO3Grid, SO3Linear, SO3VstpGrid
 from .utils import so2_expand_index, so3_expand_index
 
 __all__ = [
@@ -14,4 +14,5 @@ __all__ = [
     "SO2ComplexMul",
     "so2_expand_index", 
     "so3_expand_index",
+    "SO3VstpGrid",
 ]
