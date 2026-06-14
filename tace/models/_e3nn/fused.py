15beb2c675e0d1e3d97ae211e985f7b8a507457e
xuzemin <xvzemin123@outlook.com>
2026-06-14 17:58:50 +0800

    opt code

--- tace/models/_e3nn/fused.py
+++ tace/models/_e3nn/fused.py
@@ -412,6 +412,14 @@ class AttentionSO2TensorProduct(torch.nn.Module):
             tensor_act=tensor_act,
         )        
         if self.use_so2_edge_ace:
+            self.linear_glu = uvSO2Linear(
+                mmax,
+                lmax,
+                self.num_channel * 2,
+                self.edge_ace_hidden if self.use_so2_edge_ace else self.edge_wise_hidden,   
+                num_components_out=num_components_out,
+                weight_type=self.so2_linear_type,
+            )
             self.ece = ComplexProductBasisV2(
                 mmax, 
                 lmax, 
@@ -533,10 +541,11 @@ class AttentionSO2TensorProduct(torch.nn.Module):
 
         if self.use_so2_edge_ace:
             coefs = self.nonlinearity.scalar_act(self.linear_coefs(m_ij).squeeze(-1))
+            m_ij_2 = self.linear_glu(m_ij)
             m_ij = self.linear_up(m_ij) 
             gate = m_ij.narrow(1, 0, self.split_list[0])
             m_ij = m_ij.narrow(1, self.split_list[0], self.split_list[1])
-            m_ij = m_ij + self.nonlinearity(m_ij, gate) + self.ece(m_ij, m_ij, coefs) # x + x**2 + x**3
+            m_ij = m_ij + self.nonlinearity(m_ij, gate) + self.ece(m_ij, m_ij_2, coefs) # x + x**2 + x**3
         else:
             m_ij = self.linear_up(m_ij) 
             gate = m_ij.narrow(1, 0, self.split_list[0])
