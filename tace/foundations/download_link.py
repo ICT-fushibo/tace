41f3dc84200b2ab78f460bb7b3b4f923f68f46ba
Zemin Xu <you@example.com>
2026-07-13 03:40:08 +0800

    Add new link for foundation model

--- tace/foundations/download_link.py
+++ tace/foundations/download_link.py
@@ -18,8 +18,10 @@ from ..utils._global import CACHE_DIR
 OAM_SERIES = {
     "TACE-OMat24-1M": "https://huggingface.co/xvzemin/tace-foundations/resolve/main/TACE-OMat24-1M.pt",
     "TACE-OMat24-L": "https://huggingface.co/xvzemin/tace-foundations/resolve/main/TACE-OMat24-L.pt",
+    "TACE-OMat24-RRA-Preview": "https://huggingface.co/xvzemin/tace-foundations/resolve/main/TACE-OMat24-RRA-Preview.pt",
     "TECE-OMat24-RRA": "https://huggingface.co/xvzemin/tace-foundations/resolve/main/TECE-OMat24-RRA.pt",
 
+    "TACE-OAM-1M": "https://huggingface.co/xvzemin/tace-foundations/resolve/main/TACE-OAM-1M.pt",
     "TACE-OAM-L": "https://huggingface.co/xvzemin/tace-foundations/resolve/main/TACE-OAM-L.pt",
     "TACE-OAM-RRA-Preview": "https://huggingface.co/xvzemin/tace-foundations/resolve/main/TACE-OAM-RRA-Preview.pt",
     "TECE-OAM-RRA-1.0": "https://huggingface.co/xvzemin/tace-foundations/resolve/main/TECE-OAM-RRA-1.0.pt",
